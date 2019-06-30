3b8c65214d666712b4d8900385afc6a6eba610db
Raymond Akornor <raymondakornor@gmail.com>
2019-06-30 20:06:56 +0000

    add message model.

---
+++ BetterSearch/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  BetterSearch
+//
+//  Created by Raymond Akornor on 6/30/19.
+//  Copyright © 2019 FiniteLoop. All rights reserved.
+//
+
+import Foundation
+
+
+struct Message {
+    var text: String?
+    var date: String?
+    var id: String?
+}
