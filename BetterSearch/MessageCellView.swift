6dba4405e2eacf210695b187291d8e72bf807308
Raymond Akornor <raymondakornor@gmail.com>
2019-06-30 20:07:20 +0000

    add custom table cell view.

---
+++ BetterSearch/MessageCellView.swift
@@ -0,0 +1,15 @@
+//
+//  MessageCellView.swift
+//  BetterSearch
+//
+//  Created by Raymond Akornor on 6/30/19.
+//  Copyright © 2019 FiniteLoop. All rights reserved.
+//
+
+import Cocoa
+
+class MessageCellView: NSTableCellView {
+
+    @IBOutlet var numberTextField: NSTextField!
+    @IBOutlet var detailedTextField: NSTextField!
+}
