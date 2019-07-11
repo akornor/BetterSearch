23fd07209d4d5fe54375088872fd723fb5c44624
Raymond Akornor <raymondakornor@gmail.com>
2019-07-11 14:09:48 +0000

    add event monitor.

---
+++ BetterSearch/EventMonitor.swift
@@ -0,0 +1,35 @@
+//
+//  EventMonitor.swift
+//  BetterSearch
+//
+//  Created by Mac on 7/11/19.
+//  Copyright © 2019 FiniteLoop. All rights reserved.
+//
+
+import Cocoa
+
+public class EventMonitor {
+    private var monitor: Any?
+    private let mask: NSEvent.EventTypeMask
+    private let handler: (NSEvent?) -> Void
+    
+    public init(mask: NSEvent.EventTypeMask, handler: @escaping (NSEvent?) -> Void) {
+        self.mask = mask
+        self.handler = handler
+    }
+    
+    deinit {
+        stop()
+    }
+    
+    public func start() {
+        monitor = NSEvent.addGlobalMonitorForEvents(matching: mask, handler: handler)
+    }
+    
+    public func stop() {
+        if monitor != nil {
+            NSEvent.removeMonitor(monitor!)
+            monitor = nil
+        }
+    }
+}
